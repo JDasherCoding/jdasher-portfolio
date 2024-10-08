74058015293bfd49d08055745d5e226b01275b82
James D <66693959+James-Da@users.noreply.github.com>
2024-10-08 11:32:01 +0200

    changed rotation for inner circle

--- components/Chevron.jsx
+++ components/Chevron.jsx
@@ -27,6 +27,32 @@ const Chevron = () => {
 						viewBox="0 0 506 506"
 						xmlns="http://www.w3.org/2000/svg"
 					>
+						{/* Outer Circle */}
+						<motion.circle
+							cx="253"
+							cy="253"
+							r="250"
+							stroke="#00ff99"
+							strokeWidth="4"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							initial={{ strokeDasharray: "24 10 0 0 " }}
+							animate={{
+								strokeDasharray: [
+									"15 120 25 25 ",
+									"16 25 92 72",
+									"4 250 22 22",
+								],
+								rotate: [120, 360],
+							}}
+							transition={{
+								duration: 20,
+								repeat: Infinity,
+								repeatType: "reverse",
+							}}
+						/>
+
+						{/* Middle Circle  */}
 						<motion.circle
 							cx="253"
 							cy="253"
@@ -50,6 +76,7 @@ const Chevron = () => {
 								repeatType: "reverse",
 							}}
 						/>
+						{/* Inner Circle  */}
 						<motion.circle
 							cx="253"
 							cy="253"
@@ -65,7 +92,7 @@ const Chevron = () => {
 									"16 25 92 72",
 									"4 250 22 22",
 								],
-								rotate: [320, 160],
+								rotate: [160, 320],
 							}}
 							transition={{
 								duration: 20,
@@ -82,27 +109,7 @@ const Chevron = () => {
 					fill="transparent"
 					viewBox="0 0 506 506"
 					xmlns="http://www.w3.org/2000/svg"
-				>
-					<motion.circle
-						cx="253"
-						cy="253"
-						r="250"
-						stroke="#00ff99"
-						strokeWidth="4"
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						initial={{ strokeDasharray: "24 10 0 0 " }}
-						animate={{
-							strokeDasharray: ["15 120 25 25 ", "16 25 92 72", "4 250 22 22"],
-							rotate: [120, 360],
-						}}
-						transition={{
-							duration: 20,
-							repeat: Infinity,
-							repeatType: "reverse",
-						}}
-					/>
-				</motion.svg>
+				></motion.svg>
 			</motion.div>
 		</div>
 	);
